1c555cc68b4640de212e06f4d39a2bec8bbb3e7f
Peter Clifton <peter@clifton-electronics.co.uk>
2017-02-11 14:40:54 +0000

    Update licenses (GPLv2+ -> LGPL2.1+) and my email address

--- libopenaltium/schlib.c
+++ libopenaltium/schlib.c
@@ -1,20 +1,20 @@
 /*
  *  openaltium is a set of tools for opening Altium (TM) library files
- *  Copyright (C) 2016  Peter Clifton <pcjc2@cam.ac.uk>
+ *  Copyright (C) 2016  Peter Clifton <Peter.Clifton@clifton-electronics.co.uk>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
+ *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <stdint.h>
